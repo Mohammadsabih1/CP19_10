df34135a5cebad588d5187ec5158ddb30fba950d
Moizkaleem <49691512+Moizkaleem@users.noreply.github.com>
2019-04-20 23:37:29 +0500

    Add files via upload

---
+++ hangman.py.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# program <hangman>
+# coding by Abdul Moiz
+
+hang="       HANGMAN"
+def draw_man():
+    
+    zero = ("          |")
+    half= ("          v ")
+    one = (" ________________")
+    two = (" | /      |")
+    three = (" |/       o")
+    four = (" |      |-+-|")
+    five = (" |      | | |")
+    six = (" |        |")
+    sev =  (" |       | |")
+    eig =  (" |       | |")
+    nine = (" |    ")
+    ten = (" |  ")
+    print(hang)
+    print(zero)
+    print(half)
+    print(one)
+    print(two)
+    print(three)
+    print(four)
+    print(five)
+    print(six)
+    print(sev)
+    print(eig)
+    print(nine)
+    print(ten)
+draw_man()
+
+
+# In[ ]:
+
+
+
+
