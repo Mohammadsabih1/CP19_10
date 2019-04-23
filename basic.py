18eb5b0f9876c8ba15fc01b4f4f6ec9f4eade118
SP19-BSCS-0120 <SP19-BSCS-0120@jinnah.edu>
2019-04-23 13:55:46 +0500

    My First

---
+++ basic.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import turtle
+a=turtle.Turtle()
+a.pencolor('blue')
+a.pensize(5)
+a.fillcolor('red')
+a.begin_fill()
+a.circle(100)
+a.end_fill()
+#a.done()
+turtle.exitonclick()
+© 2019 GitHub, Inc.
+
