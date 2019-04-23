fc5ce8effe5ed15a78ef0e56d988e4376898b41b
Mohammadsabih1 <49730908+Mohammadsabih1@users.noreply.github.com>
2019-04-23 10:13:41 +0500

    my first

--- 1.py
+++ 1.py
@@ -1,19 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 #Program (id shape)
 #Written By (Mohammad Sabih)
 
-n=int(input("enter number="))
-def triangle_pattren():
-	for i in range(1,n):
-    		for j in range(1,n-i):
-        		print(" ",end=" ")
-    		for j in range (1,1*i):
-        		print("8","3",end=" ")
-   	 	print("8")
-triangle_pattren()
-
+for i in range(1,6):
+    for j in range(1,7-i):
+        print(" ",end=" ")
+    for j in range (1,1*i):
+        print("4","3",end=" ")
+    print("4")  
