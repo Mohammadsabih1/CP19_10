5d1939f3d8acfe93474eea171cd3c342e5bdd0f9
Mohammadsabih1 <49730908+Mohammadsabih1@users.noreply.github.com>
2019-04-23 10:20:24 +0500

    my second

---
+++ 6.py
@@ -0,0 +1,7 @@
+#program(February days)
+#Written by(Mohammad Sabih)
+
+print("yeasrs","\t", "oceans level")
+for current_year in range (1,26):
+    oceans_level=current_year*1.6
+    print(current_year,"\t",oceans_level)
