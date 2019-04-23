45657704de6cb6ec0b798b2d9189e184f6837a32
SP19-BSSE-0083 <SP19-BSSE-0083@jinnah.edu>
2019-04-23 14:22:48 +0500

    My first

---
+++ hangman1.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+hang="       HANGMAN"
+def draw_man():
+    
+    zero = ("          |")
+    half= ("          v ")
+    one = (" ________________")
+    two = (" | /      |")
+    three = (" |/       o")
+    four = (" |      |-+-|")
+    five = (" |      | | |")
+    six = (" |        |")
+    sev =  (" |       | |")
+    eig =  (" |       | |")
+    nine = (" |    ")
+    ten = (" |  ")
+    print(hang)
+    print(zero)
+    print(half)
+    print(one)
+    print(two)
+    print(three)
+    print(four)
+    print(five)
+    print(six)
+    print(sev)
+    print(eig)
+    print(nine)
+    print(ten)
+draw_man()
+
